0def779bbcc98a71753619da3511cc8a320da0ba
lnx7 <linixhackr@gmail.com>
2019-02-26 07:21:45 +0530

    requirements.txt added

--- novusapp/settings.py
+++ novusapp/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'novusapp/static')
 ]
-
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
